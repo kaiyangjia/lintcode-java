5510c0373b7d84ceeb915611ec6498862a6d20db
kaiyangjia <kaiyangjia@163.com>
2020-05-29 09:20:43 +0800

    add no 491,not done

---
+++ src/main/java/com/jiakaiyang/lintcode/java/lintcode/No491PalindromeNumber.java
@@ -0,0 +1,37 @@
+package com.jiakaiyang.lintcode.java.lintcode;
+
+/**
+ * 491. 回文数
+ * https://www.lintcode.com/problem/palindrome-number/description
+ * <p>
+ * 判断一个正整数是不是回文数。
+ * <p>
+ * 回文数的定义是，将这个数反转之后，得到的数仍然是同一个数。
+ * <p>
+ * Created by jia on 29/5/2020
+ * github: https://github.com/kaiyangjia
+ */
+public class No491PalindromeNumber {
+
+    /**
+     * @param num: a positive number
+     * @return: true if it's a palindrome or false
+     */
+    public static boolean isPalindrome(int num) {
+        // write your code here
+    }
+
+    public static boolean solution1(int num) {
+        if (num < 10) {
+            return true;
+        }
+        int highNum = num, lowNum = num;
+
+        while (highNum == 0 || lowNum == 0) {
+            // 获取高位数字
+
+            // 获取低位数字
+        }
+    }
+
+}

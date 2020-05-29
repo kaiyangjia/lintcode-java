763420b8803a8f64a61969da51a3c334f0b32b6b
kaiyangjia <kaiyangjia@163.com>
2020-05-29 23:30:24 +0800

    done no491

--- src/main/java/com/jiakaiyang/lintcode/java/lintcode/No491PalindromeNumber.java
+++ src/main/java/com/jiakaiyang/lintcode/java/lintcode/No491PalindromeNumber.java
@@ -12,6 +12,8 @@ package com.jiakaiyang.lintcode.java.lintcode;
  * github: https://github.com/kaiyangjia
  */
 public class No491PalindromeNumber {
+    final static int[] sizeTable = {9, 99, 999, 9999, 99999, 999999, 9999999,
+            99999999, 999999999, Integer.MAX_VALUE};
 
     /**
      * @param num: a positive number
@@ -19,19 +21,78 @@ public class No491PalindromeNumber {
      */
     public static boolean isPalindrome(int num) {
         // write your code here
+        return solution1(num);
     }
 
+    /**
+     * 1. 获取当前数字为位数 size
+     * 2. 遍历 size/2 次，分别用两个指针从高位和低位同时像中间遍历，获取每一对最高位和最低位的数值h 和 l
+     * 3. 如果出现 h 和 l不相等的情况，则说明不是回文数字；
+     * 4. 否则则为回文数字
+     *
+     * @param num
+     * @return
+     */
     public static boolean solution1(int num) {
         if (num < 10) {
             return true;
         }
+        int size = sizeOf(num);
+        int half = size / 2;
+
         int highNum = num, lowNum = num;
 
-        while (highNum == 0 || lowNum == 0) {
-            // 获取高位数字
+        for (int i = 0; i < half; i++) {
+            int h = getHighNum(highNum, size - i);
+            int l = getLowNum(lowNum);
 
-            // 获取低位数字
+            if (h != l) {
+                return false;
+            }
+
+            // 用于计算高位的下一个
+            highNum -= h * (int) Math.pow(10, size - i - 1);
+            lowNum /= 10;
         }
+
+        return true;
     }
 
+    /**
+     * 获取最高位的数值
+     *
+     * @param num
+     * @return
+     */
+    public static int getHighNum(int num, int size) {
+        int i = (int) Math.pow(10, size - 1);
+
+        int result = num / i;
+        return result;
+    }
+
+    /**
+     * 获取最低位的数值
+     *
+     * @param num
+     * @return
+     */
+    public static int getLowNum(int num) {
+        return num % 10;
+    }
+
+    /**
+     * 获取 int 的位数
+     *
+     * @param num
+     * @return
+     */
+    public static int sizeOf(int num) {
+        for (int i = 0; ; i++) {
+            if (num <= sizeTable[i]) {
+
+                return i + 1;
+            }
+        }
+    }
 }
